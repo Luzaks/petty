a16961a24651a066f6cc56889dd9ce6ca3eb880e
Luzaks <kanemekanik@gmail.com>
2023-08-21 07:28:20 -0600

    feat: add filters styling

--- src/views/dashboard/components/table/index.tsx
+++ src/views/dashboard/components/table/index.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
+import ClearIcon from '@mui/icons-material/Clear';
+import AddIcon from '@mui/icons-material/Add';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import { DashTableProps } from './index.interfaces';
 import { Contents } from '../../../../models';
 import './index.css';
 import { UserProps } from '../../../../models/state';
+
 interface FilterProps {
     filtersOpen: boolean;
     state: string[]; 
@@ -76,6 +80,25 @@ const DashTable = (dashTableProps: DashTableProps) => {
         }
     };
 
+    const handleEraseCountry = (filter: string) => {
+        if (countryFilter.length > 0) {
+            const result = countryFilter.filter((item) => item !== filter);
+            setCountryFilter([...result]);
+        }
+    };
+    const handleEraseState = (filter: string) => {
+        if (stateFilter.length > 0) {
+            const result = stateFilter.filter((item) => item !== filter);
+            setStateFilter([...result]);
+        }
+    };
+    const handleEraseGenre = (filter: string) => {
+        if (genreFilter.length > 0) {
+            const result = genreFilter.filter((item) => item !== filter);
+            setGenreFilter([...result]);
+        }
+    };
+
     const handleOnChangeCountry = (ev: any) => {
         setCountryValue(ev.target.value);
     };
@@ -101,17 +124,21 @@ const DashTable = (dashTableProps: DashTableProps) => {
         <div className="dash-table-container">
             <div className='dash-table-filters-container'>
                 <div className='flex flex-col w-fit'>
-                    <div className='flex items-center justify-start'>
-                        <button onClick={handleOpenFilters} className='filter-button w-[65px] filter-button-white'>
+                    <div className='flex flex-col md:flex-row md:items-center md:justify-start mb-2'>
+                        <button onClick={handleOpenFilters} className={filtersApplied ? 'filter-button h-[30px] w-fit filter-button-white outline outline-2 outline-gray-100' : 'filter-button h-[30px] w-fit filter-button-white'}>
+                            <FilterListIcon className='mr-1' sx={{ fontSize: 17}}></FilterListIcon>
                             Filters
                         </button>
-                        <div>
+                        <div className='flex ml-0 md:ml-2 items-center mt-2 md:mt-0'>
                             {
                                 countryFilter.length > 0 && (
-                                    <div>
+                                    <div className='flex items-center'>
                                         {
                                             countryFilter.map((item, index) => (
-                                                <button key={index} onClick={handleChangeCountry}>{item}</button>
+                                                <button className='filter-button user-table-filter' key={index} onClick={() => handleEraseCountry(item)}>
+                                                    pais:{item}
+                                                    <ClearIcon className='ml-1' sx={{ fontSize: 17 }}></ClearIcon>
+                                                </button>
                                             ))
                                         }
                                     </div>
@@ -120,10 +147,14 @@ const DashTable = (dashTableProps: DashTableProps) => {
                             }
                              {
                                 stateFilter.length > 0 && (
-                                    <div>
+                                    <div className='flex items-center'>
                                         {
                                             stateFilter.map((item, index) => (
-                                                <button key={index} onClick={handleChangeCountry}>{item}</button>
+                                                <button className='filter-button user-table-filter' key={index} onClick={() => handleEraseState(item)}>
+                                                    estado:{item}
+                                                    <ClearIcon className='ml-1' sx={{ fontSize: 17 }}></ClearIcon>
+                                                </button>
+                                                
                                             ))
                                         }
                                     </div>
@@ -132,10 +163,13 @@ const DashTable = (dashTableProps: DashTableProps) => {
                             }
                              {
                                 genreFilter.length > 0 && (
-                                    <div>
+                                    <div className='flex items-center'>
                                         {
                                             genreFilter.map((item, index) => (
-                                                <button key={index} onClick={handleChangeCountry}>{item}</button>
+                                                <button className='filter-button user-table-filter' key={index} onClick={() => handleEraseGenre(item)}>
+                                                    género:{item}
+                                                    <ClearIcon className='ml-1' sx={{ fontSize: 17 }}></ClearIcon>
+                                                </button>
                                             ))
                                         }
                                     </div>
@@ -144,23 +178,23 @@ const DashTable = (dashTableProps: DashTableProps) => {
                             }
                         </div>
                     </div>
-                  
-                    {
-                        filtersApplied && (
-                            <div className='flex items-center w-full'>
-                                <input type="text" name="genre" onChange={handleOnChangeGenre} value={genreValue} onKeyDown={handleChangeGenre} placeholder='Género' />
-                                <input type="text" name="country" onChange={handleOnChangeCountry} value={countryValue} onKeyDown={handleChangeCountry} placeholder='País' />
-                                <input type="text" name="state" onChange={handleOnChangeState} value={stateValue} onKeyDown={handleChangeState} placeholder='Estado' />
-                            </div>
-                        )
-                    }
                 </div>
-                <div>
-                    <button className='filter-button bg-blue-600 text-white' onClick={handleAddModal}>
-                        + Nuevo Usuario
+                <div className='self-start'>
+                    <button className='filter-button bg-blue-600 h-[30px] w-[30px] md:w-fit text-white' onClick={handleAddModal}>
+                       <AddIcon className='md:mr-1' sx={{ fontSize: 17 }}></AddIcon>
+                        <span className='hidden md:inline'>Nuevo Usuario</span>
                     </button>
                 </div>
             </div>
+            {
+                        filtersApplied && (
+                            <div className='text-xs flex items-center w-full mb-2 flex-col md:flex-row'>
+                                <input className='user-table-filter-input' type="text" name="genre" onChange={handleOnChangeGenre} value={genreValue} onKeyDown={handleChangeGenre} placeholder='Género' />
+                                <input className='user-table-filter-input' type="text" name="country" onChange={handleOnChangeCountry} value={countryValue} onKeyDown={handleChangeCountry} placeholder='País' />
+                                <input className='user-table-filter-input' type="text" name="state" onChange={handleOnChangeState} value={stateValue} onKeyDown={handleChangeState} placeholder='Estado' />
+                            </div>
+                        )
+                    }
             <table className="dash-table-component">
                 <thead className='dash-table-t-head'>
                     <tr>
